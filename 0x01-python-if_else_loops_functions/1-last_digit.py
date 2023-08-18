7d4bbf970ff2c891b4c5d4e3acdeec549257eebf
Soumaiald <soumaialoud@gmail.com>
2023-08-18 04:35:04 -0700

    adding 0 to 7 files

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_num = int(str(number)[-1])
+str1 = "Last digit of " + str(number) + " is " + str(last_num) + " and is "
+if last_num>5:
+    print(str1 + "greater than 5")
+elif last_num == 0:
+    print(str1 + "0")
+else:
+    print(str1 + "less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def isupper(c):
+    if ord(c) in range(65, 91):
+        return True
+    else:
+        return False
