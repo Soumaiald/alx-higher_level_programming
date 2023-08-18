7b3083680a9850303342e3e0649bc36ae77b4213
Soumaiald <soumaialoud@gmail.com>
2023-08-18 09:18:51 -0700

    corr 0x02

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,9 +3,10 @@
 if __name__ == "__main__":
     """Print sum  of argv"""
     import sys
+
     count = 0
     for i in range(1, len(sys.argv)):
-        count = count + int(sys.argv[i])
+        count += int(sys.argv[i])
     print("{}".format(count))
 
         
